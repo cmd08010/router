6b881b99955423f38222b44eb09c215b4b62c1a0
cmd08010 <colleendunion@gmail.com>
2020-02-02 18:27:51 -0500

    updating

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { getHash } from "./utils/utils"
 import Users from "./components/Users"
 import Home from "./components/Home"
 import Nav from "./components/Nav"
+import axios from "axios"
 
 function App() {
   const [params, setParams] = useState(qs.parse(getHash()))
@@ -15,11 +16,21 @@ function App() {
     setParams(qs.parse(getHash()))
   }, [])
 
+  let [users, setUsers] = useState([])
+
+  const data = axios.get("https://acme-users-api-rev.herokuapp.com/api/users")
+  let usersData = data.then(response => {
+    let usersDataRespones = response.data.users
+  })
+  console.log(usersDataResponses)
+
   return (
     <div className="App">
       <Nav />
       {params.view === undefined && <Home />}
-      {params.view === "users" && <Users />}
+      {params.view === "users" && (
+        <Users setUsers={setUsers} usersData={usersData} />
+      )}
     </div>
   )
 }
--- src/components/Users.js
+++ src/components/Users.js
@@ -1,5 +1,20 @@
 import React from "react"
+import axios from "axios"
 
-export default function Users() {
-  return <div>Users</div>
+export default function Users({ users, setUsers, usersData }) {
+  console.log(users)
+  let names = [usersData]
+  names.forEach(user => {
+    console.log(user)
+  })
+  return (
+    <div>
+      Users
+      <ul>
+        {users.map(user => {
+          return <li>{user.fullName}</li>
+        })}
+      </ul>
+    </div>
+  )
 }
